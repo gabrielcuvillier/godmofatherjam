0c0ac2d9bce71ad3f79d3550d71b058392d70012
Chloe-Ms <mllechloe.mathias@gmail.com>
2025-09-03 09:37:31 +0200

    Health script and treasure prefab

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    float _currentHealth;
+
+    [SerializeField] float _maxHealth = 3f;
+
+    //Old health value / New health value
+    public event Action<float,float> OnHealthChange;
+    public event Action OnDeath;
+
+    private void Awake()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage (float damage)
+    {
+        float newHealth = Mathf.Max(_currentHealth - damage, 0f);
+        OnHealthChange?.Invoke(_currentHealth, newHealth);
+        _currentHealth = newHealth;
+
+        if (_currentHealth < 0f)
+        {
+            OnDeath?.Invoke();
+        }
+    }
+
+    public void Heal(float healValue)
+    {
+        float newHealth = Mathf.Min(_currentHealth + healValue, _maxHealth);
+        OnHealthChange?.Invoke(_currentHealth, newHealth);
+        _currentHealth = newHealth;
+    }
+}
