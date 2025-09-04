17b02d233a8669ea786245aeca2dadd46fe1fc14
BRISSY Axel <axelbrissy0804@gmail.com>
2025-09-04 22:11:53 +0200

    skeleton animation

--- Assets/Scripts/Monstre/IABase.cs
+++ Assets/Scripts/Monstre/IABase.cs
@@ -1,8 +1,10 @@
 using NUnit.Framework;
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Health))]
+[RequireComponent(typeof(Animator))]
 public class IABase : MonoBehaviour
 {
     public enum EIAState
@@ -14,12 +16,24 @@ public class IABase : MonoBehaviour
 
     private Rigidbody rb;
     private Health targetHealth;
+    private Animator animator;
     [SerializeField] private Transform target;
     public Transform Target
     {
         get { return target; }
         set { target = value; }
     }
+
+    [Header("Audio")]
+    [Tooltip("Boucle jouée quand l'ennemi se déplace")]
+    [SerializeField] private AudioClip movementLoopClip;
+    [Tooltip("Son joué quand l'ennemi se fait toucher")]
+    [SerializeField] private AudioClip hitClip;
+
+    private AudioSource movementSource;
+    private AudioSource sfxSource;
+    private bool hitOverrideActive = false;
+
 
     [Header("IA Settings")]
     [SerializeField] private EIAState currentState;
@@ -47,9 +61,21 @@ public class IABase : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
 
         attackCooldown = 1f / attackSpeed;
         attackTimer = 0f;
+
+        movementSource = gameObject.AddComponent<AudioSource>();
+        movementSource.playOnAwake = false;
+        movementSource.loop = true;
+        movementSource.clip = movementLoopClip;
+        movementSource.spatialBlend = 1f;
+
+        sfxSource = gameObject.AddComponent<AudioSource>();
+        sfxSource.playOnAwake = false;
+        sfxSource.loop = false;
+        sfxSource.spatialBlend = 1f;
     }
 
     private void Start()
@@ -58,6 +84,7 @@ public class IABase : MonoBehaviour
         {
             targetHealth = target.transform.parent.GetComponent<Health>();
         }
+        animator.SetBool("IsMoving", true);
     }
 
     void Update()
@@ -96,11 +123,40 @@ public class IABase : MonoBehaviour
         Destroy(gameObject);
     }
 
+    public void Hit()
+    {
+        animator.SetTrigger("Hit");
+        if (hitClip != null)
+            StartCoroutine(PlayHitAndReturn());
+    }
+
+    private IEnumerator PlayHitAndReturn()
+    {
+        hitOverrideActive = true;
+
+        if (movementSource.isPlaying) movementSource.Pause();
+
+        sfxSource.Stop();
+        sfxSource.clip = hitClip;
+        sfxSource.Play();
+
+        yield return new WaitForSeconds(hitClip.length);
+
+        hitOverrideActive = false;
+
+        if (animator.GetBool("IsMoving") && movementLoopClip != null)
+        {
+            if (!movementSource.isPlaying)
+                movementSource.UnPause();
+        }
+    }
+
     private void Move()
     {
         if (target != null && IsTargetInRange())
         {
             currentState = EIAState.Attack;
+            animator.SetBool("IsMoving", false);
             return;
         }
         Vector3 direction = GetDirectionToTarget();
@@ -108,6 +164,32 @@ public class IABase : MonoBehaviour
         {
             float currentMovementSpeed = currentTerrain == TerrainType.Sand ? movementSpeedSand : movementSpeedWater;
             rb.MovePosition(rb.position + direction * currentMovementSpeed * Time.deltaTime);
+
+            if (!animator.GetBool("IsMoving"))
+            {
+                animator.SetBool("IsMoving", true);
+                UpdateMovementAudio(true);
+            }
+        }
+    }
+
+    private void UpdateMovementAudio(bool shouldMove)
+    {
+        if (hitOverrideActive) return;
+
+        if (shouldMove && movementLoopClip != null)
+        {
+            if (!movementSource.isPlaying)
+            {
+                if (movementSource.time > 0f)
+                    movementSource.UnPause();
+                else
+                    movementSource.Play();
+            }
+        }
+        else
+        {
+            if (movementSource.isPlaying) movementSource.Pause();
         }
     }
 
@@ -129,6 +211,7 @@ public class IABase : MonoBehaviour
             if (!IsTargetInRange())
             {
                 currentState = EIAState.Chase;
+                animator.SetBool("IsMoving", true);
             }
         }
     }
